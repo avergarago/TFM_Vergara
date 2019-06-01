cbbfda0e2e0fb418293e2ffc2effb492f2c55e34
avergarago <35374177+avergarago@users.noreply.github.com>
2019-06-01 19:34:09 +0200

    Create Bowtie2_MetaSPAdes.sh
    Mapeo de la secuencias con el index

---
+++ BOWTIE2/MetaSPAdes/Bowtie2_MetaSPAdes.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+#$ -N bowtie2_script
+#$ -q imppcv3
+#$ -pe smp 8
+#$ -V
+#$ -e /imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/Bowtie2/MetaSPAdes
+#$ -o /imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/Bowtie2/MetaSPAdes
+
+aux="XXX"
+file="/imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/Filtered/$aux"
+Bowtie2_Mapping="/imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/Bowtie2/MetaSPAdes/${aux%%_R1_paired.fq.gz}.sorted.bam"
+bowtie2 -t -x $ISHARE/Work/Projects/TFM_Andrea/Bowtie2/MetaSPAdes/Reference/AllCommonGenesINDEX -1 $file -2 ${file%%_R1_paired.fq.gz}_R2_paired.fq.gz --very-sensitive-local -p 8 | samtools view -@ 8 -F 4 -q 10 -S -b - | samtools sort -@ 8 -o ${Bowtie2_Mapping}
