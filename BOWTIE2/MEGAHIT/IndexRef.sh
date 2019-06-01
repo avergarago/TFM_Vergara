7d12a9d997363794380026ec669190c624263aa5
avergarago <35374177+avergarago@users.noreply.github.com>
2019-06-01 19:30:58 +0200

    Create IndexRef.sh
    Indexado de los genes de referencia

---
+++ BOWTIE2/MEGAHIT/IndexRef.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+bowtie2-build /imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/PROKKA/MEGAHIT/AllCommonGenes.fasta /imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/Bowtie2/MEGAHIT/Reference/AllCommonGenesINDEX
