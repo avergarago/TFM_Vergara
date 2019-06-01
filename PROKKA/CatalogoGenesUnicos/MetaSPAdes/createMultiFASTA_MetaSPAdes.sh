424e92c8c37d343229cba6f3f67e2af4f629eff2
avergarago <35374177+avergarago@users.noreply.github.com>
2019-06-01 19:28:07 +0200

    Update createMultiFASTA_MetaSPAdes
    En este caso, lo hago en un único paso.

--- PROKKA/CatalogoGenesUnicos/MetaSPAdes/createMultiFASTA_MetaSPAdes.sh
+++ PROKKA/CatalogoGenesUnicos/MetaSPAdes/createMultiFASTA_MetaSPAdes.sh
@@ -1,1 +1,23 @@
+#! /bin/bash
+
+#$ -N create_multifasta
+#$ -q imppc
+#$ -pe smp 8
+#$ -V
+#$ -e /imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/PROKKA/MetaSPAdes
+#$ -o /imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/PROKKA/MetaSPAdes
+
+FILES="/imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/PROKKA/MetaSPAdes/Sample_0*/*.ffn"
+
+for file in $FILES
+
+do
+
+        cat /imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/PROKKA/MetaSPAdes/temp.fasta  $file  | fasta_formatter | fastx_collapser > /imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/PROKKA/MetaSPAdes/temp2.fasta
+
+        mv /imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/PROKKA/MetaSPAdes/temp2.fasta /imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/PROKKA/MetaSPAdes/temp.fasta
+
+done
+
+cp /imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/PROKKA/MetaSPAdes/temp.fasta /imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/PROKKA/MetaSPAdes/AllCommonGenes.fasta
 
