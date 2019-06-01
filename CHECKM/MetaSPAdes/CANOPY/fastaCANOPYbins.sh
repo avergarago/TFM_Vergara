be7713affc41e410569e3020ac9bd66df3ff3867
avergarago <35374177+avergarago@users.noreply.github.com>
2019-06-01 20:35:26 +0200

    Create fastaCANOPYbins.sh

---
+++ CHECKM/MetaSPAdes/CANOPY/fastaCANOPYbins.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+#$ -S /bin/bash
+#$ -N FASTA_canopy_script
+#$ -q imppc
+#$ -pe smp 8
+#$ -V
+#$ -e /imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/CANOPY/MetaSPAdes/bins_dir/fasta
+#$ -o /imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/CANOPY/MetaSPAdes/bins_dir/fasta
+
+
+FILES="/imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/CANOPY/MetaSPAdes/bins_dir/CAG*"
+
+for file in $FILES
+
+do
+    	filename=`basename $file`
+    	faSomeRecords /imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/PROKKA/MetaSPAdes/AllCommonGenes.fasta $file /imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/CANOPY/MetaSPAdes/bins_dir/fasta/${filename%%.txt}.txt
+
+done
