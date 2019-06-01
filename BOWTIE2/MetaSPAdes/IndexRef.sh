16a9ae51e310eed7ae4ec302d8f829785308734e
avergarago <35374177+avergarago@users.noreply.github.com>
2019-06-01 19:33:38 +0200

    Create IndexRef.sh
    Indexado de los genes de referencia

---
+++ BOWTIE2/MetaSPAdes/IndexRef.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+bowtie2-build /imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/PROKKA/MetaSPAdes/AllCommonGenes.fasta /imppc/external/irsicaixa/share/Work/Projects/TFM_Andrea/Bowtie2/MetaSPAdes/Reference/AllCommonGenesINDEX
